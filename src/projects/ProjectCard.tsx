ec7fb6491f7fa64f16573b2c8348138c0c0cdc8e
Katya <katya.mikhaylova.98@list.ru>
2023-06-02 01:05:06 +0200

    add: form to edit data, lab 10

---
+++ src/projects/ProjectCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Project } from "./Project";
+
+interface ProjectListProps {
+  project: Project;
+}
+
+function ProjectCard({ project }: ProjectListProps) {
+  function handleEditClicked(projectBeingEdited: Project) {
+    console.log(projectBeingEdited);
+  }
+  return (
+    <div className="card">
+      <img src={project.imageUrl} alt={project.name} />
+      <section className="section dark">
+        <h5 className="strong">
+          <strong>{project.name}</strong>
+        </h5>
+        <p>{project.description}</p>
+        <p>Budget: {project.budget.toLocaleString()}</p>
+        <button className="bordered" onClick={() => handleEditClicked(project)}>
+          <span className="icon-edit"></span> Edit{" "}
+        </button>
+      </section>
+    </div>
+  );
+}
+
+export default ProjectCard;
